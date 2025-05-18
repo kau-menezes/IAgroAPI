fbf5be06a874276f0df4b759cb994e7a4197e766
kau-menezes <kau.vmenezes@gmail.com>
2025-05-18 20:38:38 -0300

    fix: null validation for update company

--- IAgro.Application/Features/Companies/Update/UpdateCompanyValidator.cs
+++ IAgro.Application/Features/Companies/Update/UpdateCompanyValidator.cs
@@ -9,15 +9,18 @@ public class UpdateCompanyValidator : AbstractValidator<UpdateCompanyRequest>
         RuleFor(c => c.Props.Name)
             .NotEmpty()
             .MinimumLength(2)
-            .MaximumLength(35);
+            .MaximumLength(35)
+            .When(c => c.Props.Name is not null);
 
         RuleFor(c => c.Props.CNPJ)
             .NotEmpty()
-            .Length(14);
+            .Length(14)
+            .When(c => c.Props.CNPJ is not null);
 
         RuleFor(c => c.Props.Country)
             .NotEmpty()
             .MinimumLength(2)
-            .MaximumLength(35);
+            .MaximumLength(35)
+            .When(c => c.Props.Country is not null);
     }
 }
