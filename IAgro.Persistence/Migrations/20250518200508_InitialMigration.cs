f4211125a82f80bf90cd91ccdeaca161855505dd
kau-menezes <kau.vmenezes@gmail.com>
2025-05-18 17:20:54 -0300

    feat: initial migrations

---
+++ IAgro.Persistence/Migrations/20250518200508_InitialMigration.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace IAgro.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:postgis", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "companies",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "varchar(35)", nullable: false),
+                    cnpj = table.Column<string>(type: "varchar(14)", nullable: false),
+                    country = table.Column<string>(type: "varchar(35)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "timestamptz", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_companies", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "devices",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    nickname = table.Column<string>(type: "varchar(35)", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "timestamptz", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_devices", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "fields",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    company_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    nickname = table.Column<string>(type: "varchar(35)", nullable: false),
+                    area = table.Column<double>(type: "double precision", nullable: false),
+                    crop = table.Column<string>(type: "varchar(35)", nullable: false),
+                    location_points = table.Column<MultiPoint>(type: "geometry (MultiPoint, 4326)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "timestamptz", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_fields", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_fields_companies_company_id",
+                        column: x => x.company_id,
+                        principalTable: "companies",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    company_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    email = table.Column<string>(type: "varchar(255)", nullable: false),
+                    password = table.Column<string>(type: "varchar(255)", nullable: false),
+                    role = table.Column<short>(type: "smallint", nullable: false, defaultValue: (short)0),
+                    created_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "timestamptz", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_users_companies_company_id",
+                        column: x => x.company_id,
+                        principalTable: "companies",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "field_scans",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    field_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "timestamptz", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_field_scans", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_field_scans_fields_field_id",
+                        column: x => x.field_id,
+                        principalTable: "fields",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "crop_diseases",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    field_scan_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    disease = table.Column<string>(type: "varchar(255)", nullable: false),
+                    detected_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    location_point = table.Column<Point>(type: "geometry (Point, 4326)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "timestamptz", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_crop_diseases", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_crop_diseases_field_scans_field_scan_id",
+                        column: x => x.field_scan_id,
+                        principalTable: "field_scans",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_companies_cnpj",
+                table: "companies",
+                column: "cnpj",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_companies_name",
+                table: "companies",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_crop_diseases_field_scan_id",
+                table: "crop_diseases",
+                column: "field_scan_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_field_scans_field_id",
+                table: "field_scans",
+                column: "field_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_fields_company_id",
+                table: "fields",
+                column: "company_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_company_id",
+                table: "users",
+                column: "company_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "crop_diseases");
+
+            migrationBuilder.DropTable(
+                name: "devices");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "field_scans");
+
+            migrationBuilder.DropTable(
+                name: "fields");
+
+            migrationBuilder.DropTable(
+                name: "companies");
+        }
+    }
+}
