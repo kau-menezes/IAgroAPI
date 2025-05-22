1a2964241751a0a0dad8b14225682c2c362bd22a
kau-menezes <kau.vmenezes@gmail.com>
2025-05-22 19:32:33 -0300

    refact: device model

--- IAgro.Domain/Models/Device.cs
+++ IAgro.Domain/Models/Device.cs
@@ -2,5 +2,11 @@ namespace IAgro.Domain.Models;
 
 public class Device : BaseModel
 {
+    public required Guid CompanyId { get; set; }
+
+    public required Company Company { get; set; }
+
     public string? Nickname { get; set; }
+
+    public string? Code { get; set; }
 }
