2c6c4f92f09ed036b31d213d529f34c1b4a66074
kau-menezes <kau.vmenezes@gmail.com>
2025-05-25 14:32:25 -0300

    refat: repositories

--- IAgro.Persistence/Repositories/Devices/DevicesRepository.cs
+++ IAgro.Persistence/Repositories/Devices/DevicesRepository.cs
@@ -1,8 +1,15 @@
 using IAgro.Application.Repositories.DevicesRepository;
 using IAgro.Domain.Models;
 using IAgro.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace IAgro.Persistence.Repositories.Devices;
 
 public class DevicesRepository(IAgroContext context)
-    : BaseRepository<Device>(context), IDevicesRepository;
+    : BaseRepository<Device>(context), IDevicesRepository
+{
+    public Task<Device?> GetByCoded(string code, CancellationToken cancellationToken)
+    => context.Set<Device>()
+    .Where(d => d.DeletedAt == null && d.Code == code)
+    .FirstOrDefaultAsync(cancellationToken);
+}
